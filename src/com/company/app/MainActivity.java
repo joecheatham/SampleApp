1949cde83c4561d02795a6d7d371698892befc4d
Joe Cheatham <cheathamjoe@gmail.com>
2018-02-07 21:49:08 -0800

    init repo

---
+++ src/com/company/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.company.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
